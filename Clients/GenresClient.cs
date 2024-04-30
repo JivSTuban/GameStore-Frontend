8a65147c46a1a1b30fb1946de87e2bdeaa7d8163
JivSTuban <larsjib@gmail.com>
2024-04-30 12:11:40 +0800

    first

---
+++ Clients/GenresClient.cs
@@ -0,0 +1,17 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenresClient
+{
+    private readonly Genre[] genres = [
+        new(){
+            Id = 1,
+            Name = "Fighting"
+        }
+    ];
+
+    public Genre[] GetGenres() => genres;
+
+  
+}
